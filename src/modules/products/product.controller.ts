aa48e81efa6914f80f20a511a3492095f628d068
Tien Kim <tienkim9920@gmail.com>
2023-05-27 10:25:48 +0700

    update

--- src/modules/products/product.controller.ts
+++ src/modules/products/product.controller.ts
@@ -1,30 +1,57 @@
-import { Body, Controller, Delete, Get, Post, Put } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put, ValidationPipe } from "@nestjs/common";
+import { ProductService } from "./product.service";
+import { ResponseData } from "src/global/globalClass";
+import { HttpMessage, HttpStatus } from "src/global/globalEnum";
+import { Product } from "src/models/product.model";
+import { ProductDto } from "src/dto/product.dto";
 
 @Controller('products')
 export class ProductController {
 
+  constructor(private readonly productService: ProductService) {}
+
   @Get()
-  getProducts(): string {
-    return 'GET LIST PRODUCTS';
+  getProducts(): ResponseData<Product[]> {
+    try {
+      return new ResponseData<Product[]>(this.productService.getProducts(), HttpStatus.SUCCESS, HttpMessage.SUCCESS);
+    } catch (error) {
+      return new ResponseData<Product[]>(null, HttpStatus.ERROR, HttpMessage.ERROR);
+    }
   }
 
   @Post()
-  createProduct(): string {
-    return 'POST PRODUCT';
+  createProduct(@Body(new ValidationPipe()) productDto: ProductDto): ResponseData<ProductDto> {
+    try {
+      return new ResponseData<Product>(this.productService.createProduct(productDto), HttpStatus.SUCCESS, HttpMessage.SUCCESS);
+    } catch (error) {
+      return new ResponseData<Product>(null, HttpStatus.ERROR, HttpMessage.ERROR);
+    }
   }
 
   @Get('/:id')
-  detailProduct(): string {
-    return 'DETAIL PRODUCT';
+  detailProduct(@Param('id') id: number): ResponseData<Product> {
+    try {
+      return new ResponseData<Product>(this.productService.detailProduct(id), HttpStatus.SUCCESS, HttpMessage.SUCCESS);
+    } catch (error) {
+      return new ResponseData<Product>(null, HttpStatus.ERROR, HttpMessage.ERROR);
+    }
   }
 
   @Put('/:id')
-  updateProduct(): string {
-    return 'UPDATE PRODUCT';
+  updateProduct(): ResponseData<string> {
+    try {
+      return new ResponseData<string>(this.productService.updateProduct(), HttpStatus.SUCCESS, HttpMessage.SUCCESS);
+    } catch (error) {
+      return new ResponseData<string>(null, HttpStatus.ERROR, HttpMessage.ERROR);
+    }
   }
 
   @Delete('/:id')
-  deleteProduct(): string {
-    return 'DELETE PRODUCT';
+  deleteProduct(): ResponseData<string> {
+    try {
+      return new ResponseData<string>(this.productService.deleteProduct(), HttpStatus.SUCCESS, HttpMessage.SUCCESS);
+    } catch (error) {
+      return new ResponseData<string>(null, HttpStatus.ERROR, HttpMessage.ERROR);
+    }
   }
 }
---
+++ src/modules/products/product.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { ProductDto } from 'src/dto/product.dto';
+import { Product } from 'src/models/product.model';
+
+@Injectable()
+export class ProductService {
+
+  private products: Product[] = [
+    {id: 1, categoryId: 2, price: 80000, productName: "Keyboard"},
+    {id: 2, categoryId: 3, price: 90000, productName: "Nine Dev"},
+  ]
+
+  getProducts(): Product[] {
+    return this.products;
+  }
+
+  createProduct(productDto: ProductDto): Product {
+    const product: Product = {
+      id: Math.random(),
+      ...productDto
+    };
+    this.products.push(product);
+    return product;
+  }
+
+  detailProduct(id: number): Product {
+    return this.products.find(item => item.id === Number(id));
+  }
+
+  updateProduct(): string {
+    return 'UPDATE PRODUCT';
+  }
+
+  deleteProduct(): string {
+    return 'DELETE PRODUCT';
+  }
+}
